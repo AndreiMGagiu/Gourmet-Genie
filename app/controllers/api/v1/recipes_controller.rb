8c54d0d32914765b1b616f3ce79e0589bff19834
Andrei Gagiu <andrei.gagiu@x.builder.ai>
2024-10-20 13:29:10 +0300

    impr: recipes controller

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -3,33 +3,47 @@
 module Api
   module V1
     class RecipesController < BaseController
-      # GET /api/v1/recipes/search
-      #
-      # Searches for recipes based on the provided ingredients.
-      #
-      # @return [JSON] A JSON response containing matching recipes or an error message.
-      def search
-        return render_unprocessable_entity(['Ingredients parameter is required']) if ingredients.empty?
+      before_action :validate_ingredients
+      before_action :fetch_recipes
 
-        if recipes.present?
-          render json: { recipes: recipes }, status: :ok
-        else
-          render_not_found('Not found')
-        end
+      # Returns recipes based on provided ingredients
+      # @return [JSON] List of matching recipes with their dietary requirements
+      def search
+        render json: { recipes: }, status: :ok
       end
 
       private
 
-      # Retrieve recipes based on ingredients.
-      #
-      # @return [ActiveRecord::Relation] The filtered list of recipes.
-      def recipes
-        @recipes ||= Recipe.find_by_ingredients(ingredients) # Update query as necessary
+      # Validates the presence of the ingredients parameter
+      # Renders an unprocessable entity error if the ingredients parameter is missing
+      # @return [void]
+      def validate_ingredients
+        render_unprocessable_entity(['Ingredients parameter is required']) if ingredients.empty?
       end
 
-      # Extract and clean ingredients from the request params.
-      #
-      # @return [Array<String>] Cleaned ingredient strings.
+      # Fetches the matching recipes based on ingredients
+      # Renders a not found error if no matching recipes are found
+      # @return [void]
+      def fetch_recipes
+        render_not_found('No recipes found for the given ingredients') if match_recipes.empty?
+      end
+
+      # Retrieves the recipes matching the provided ingredients
+      # @return [Array<Recipe>] Array of Recipe objects with dietary requirements included
+      def match_recipes
+        @match_recipes ||= Recipe.find_by_ingredients(ingredients).includes(:dietary_requirements)
+      end
+
+      # Builds the recipe data including dietary requirements for the response
+      # @return [Array<Hash>] Array of hashes representing recipes with dietary requirements
+      def recipes
+        match_recipes.map do |recipe|
+          recipe.as_json.merge(dietary_requirements: recipe.dietary_requirements.pluck(:name))
+        end
+      end
+
+      # Splits and cleans the ingredients parameter
+      # @return [Array<String>] Array of cleaned ingredient strings
       def ingredients
         @ingredients ||= params[:ingredients].to_s.split(',').map(&:strip).reject(&:empty?)
       end
